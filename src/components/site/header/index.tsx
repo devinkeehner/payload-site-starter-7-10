19c618ba5f14b7d28335d0b0495ca7fcb5bbe2be
Bridger Tower <bridgertower@gmail.com>
2025-03-06 15:31:41 -0700

    header work

--- src/components/site/header/index.tsx
+++ src/components/site/header/index.tsx
@@ -1,11 +1,23 @@
-import { HeaderClient } from './index.client'
+import { Section, Container } from '@/components/craft'
+import { HeaderNav } from './nav'
+import Link from 'next/link'
 import { getCachedGlobal } from '@/lib/utilities/getGlobals'
-import React from 'react'
 
 import type { Header } from '@/payload-types'
 
 export async function Header() {
   const headerData: Header = await getCachedGlobal('header', 1)()
 
-  return <HeaderClient data={headerData} />
+  return (
+    <header className="">
+      <Section className="py-0 lg:py-0">
+        <Container className="flex justify-between">
+          <Link className="text-lg font-semibold tracking-tight" href="/">
+            Site Starter
+          </Link>
+          <HeaderNav data={headerData} />
+        </Container>
+      </Section>
+    </header>
+  )
 }
