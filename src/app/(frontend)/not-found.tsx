e1e05e2126e85a52b1c05ad457e7da2e4274f6f2
Bridger Tower <bridgertower@gmail.com>
2025-03-06 16:21:34 -0700

    not found ui

--- src/app/(frontend)/not-found.tsx
+++ src/app/(frontend)/not-found.tsx
@@ -1,18 +1,21 @@
 import Link from 'next/link'
 import React from 'react'
 
+import { Section, Container } from '@/components/layout'
 import { Button } from '@/components/ui/button'
 
 export default function NotFound() {
   return (
-    <div className="container py-28">
-      <div className="prose max-w-none">
-        <h1 style={{ marginBottom: 0 }}>404</h1>
-        <p className="mb-4">This page could not be found.</p>
-      </div>
-      <Button asChild variant="default">
-        <Link href="/">Go home</Link>
-      </Button>
-    </div>
+    <Section>
+      <Container>
+        <div className="ds spaced">
+          <h1>404</h1>
+          <p className="mb-4">This page could not be found.</p>
+        </div>
+        <Button asChild>
+          <Link href="/">Go home</Link>
+        </Button>
+      </Container>
+    </Section>
   )
 }
