9551777b28e21aeb4e6086e6a80bede7081167a5
Bridger Tower <bridgertower@gmail.com>
2025-03-07 10:34:25 -0700

    wip

--- src/components/blocks/banner-block/index.tsx
+++ src/components/blocks/banner-block/index.tsx
@@ -1,8 +1,10 @@
 import type { BannerBlock as BannerBlockProps } from 'src/payload-types'
 
+import { Section, Container } from '@/components/layout'
 import { cn } from '@/lib/utils'
-import React from 'react'
+
 import RichText from '@/components/site/rich-text'
+import React from 'react'
 
 type Props = {
   className?: string
@@ -10,17 +12,19 @@ type Props = {
 
 export const BannerBlock: React.FC<Props> = ({ className, content, style }) => {
   return (
-    <div className={cn('mx-auto my-8 w-full', className)}>
-      <div
-        className={cn('border py-3 px-6 flex items-center rounded', {
-          'border-border bg-card': style === 'info',
-          'border-error bg-error/30': style === 'error',
-          'border-success bg-success/30': style === 'success',
-          'border-warning bg-warning/30': style === 'warning',
-        })}
-      >
-        <RichText data={content} enableGutter={false} enableProse={false} />
-      </div>
-    </div>
+    <Section className={cn(className)}>
+      <Container>
+        <div
+          className={cn('border py-3 px-6 flex items-center rounded', {
+            'border-border bg-card': style === 'info',
+            'border-error bg-error/30': style === 'error',
+            'border-success bg-success/30': style === 'success',
+            'border-warning bg-warning/30': style === 'warning',
+          })}
+        >
+          <RichText data={content} enableGutter={false} enableProse={false} />
+        </div>
+      </Container>
+    </Section>
   )
 }
--- src/components/blocks/render-blocks.tsx
+++ src/components/blocks/render-blocks.tsx
@@ -35,12 +35,8 @@ export const RenderBlocks: React.FC<{
             const Block = blockComponents[blockType]
 
             if (Block) {
-              return (
-                <div className="my-16" key={index}>
-                  {/* @ts-expect-error there may be some mismatch between the expected types here */}
-                  <Block {...block} disableInnerContainer />
-                </div>
-              )
+              // @ts-ignore expected error
+              return <Block key={index} index={index} {...block} disableInnerContainer />
             }
           }
           return null
