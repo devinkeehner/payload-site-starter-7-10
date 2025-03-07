15499e8d793a4b1fd6f8a91c0d049eede88d5655
Bridger Tower <bridgertower@gmail.com>
2025-03-07 10:22:49 -0700

    content block responsive

--- src/components/blocks/content-block/index.tsx
+++ src/components/blocks/content-block/index.tsx
@@ -7,37 +7,35 @@ import type { ContentBlock as ContentBlockProps } from '@/payload-types'
 import { CMSLink } from '@/components/site/link'
 import { Container, Section } from '@/components/layout'
 
-export const ContentBlock: React.FC<ContentBlockProps> = (props) => {
+export const ContentBlock = (props: ContentBlockProps) => {
   const { columns } = props
-
-  const colsSpanClasses = {
-    full: '12',
-    half: '6',
-    oneThird: '4',
-    twoThirds: '8',
-  }
 
   return (
     <Section>
-      <Container className="grid grid-cols-4 lg:grid-cols-12 gap-y-8 gap-x-16">
-        {columns &&
-          columns.length > 0 &&
-          columns.map((col, index) => {
-            const { enableLink, link, richText, size } = col
+      <Container>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-12 gap-y-8 gap-x-4 md:gap-x-8 lg:gap-x-16">
+          {columns &&
+            columns.length > 0 &&
+            columns.map((col, index) => {
+              const { enableLink, link, richText, size } = col
 
-            return (
-              <div
-                className={cn(`col-span-4 lg:col-span-${colsSpanClasses[size!]}`, {
-                  'md:col-span-2': size !== 'full',
-                })}
-                key={index}
-              >
-                {richText && <RichText data={richText} enableGutter={false} />}
+              return (
+                <div
+                  className={cn(`col-span-1`, {
+                    'sm:col-span-2 md:col-span-4 lg:col-span-12': size === 'full',
+                    'sm:col-span-1 md:col-span-2 lg:col-span-6': size === 'half',
+                    'sm:col-span-1 md:col-span-2 lg:col-span-4': size === 'oneThird',
+                    'sm:col-span-2 md:col-span-3 lg:col-span-8': size === 'twoThirds',
+                  })}
+                  key={index}
+                >
+                  {richText && <RichText data={richText} enableGutter={false} />}
 
-                {enableLink && <CMSLink {...link} />}
-              </div>
-            )
-          })}
+                  {enableLink && <CMSLink {...link} />}
+                </div>
+              )
+            })}
+        </div>
       </Container>
     </Section>
   )
--- src/components/blocks/cta-block/index.tsx
+++ src/components/blocks/cta-block/index.tsx
@@ -4,11 +4,12 @@ import type { CallToActionBlock as CTABlockProps } from '@/payload-types'
 
 import RichText from '@/components/site/rich-text'
 import { CMSLink } from '@/components/site/link'
+import { Container, Section } from '@/components/layout'
 
 export const CallToActionBlock: React.FC<CTABlockProps> = ({ links, richText }) => {
   return (
-    <div className="container">
-      <div className="bg-card rounded border-border border p-4 flex flex-col gap-8 md:flex-row md:justify-between md:items-center">
+    <Section>
+      <Container className="bg-card rounded border p-4 flex flex-col gap-8 md:flex-row md:justify-between md:items-center">
         <div className="max-w-[48rem] flex items-center">
           {richText && <RichText className="mb-0" data={richText} enableGutter={false} />}
         </div>
@@ -17,7 +18,7 @@ export const CallToActionBlock: React.FC<CTABlockProps> = ({ links, richText }) 
             return <CMSLink key={i} size="lg" {...link} />
           })}
         </div>
-      </div>
-    </div>
+      </Container>
+    </Section>
   )
 }
