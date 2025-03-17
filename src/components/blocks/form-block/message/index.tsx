a1f19642ccbe708b51b14d7e95058fd25eb5e968
Bridger Tower <bridgertower@gmail.com>
2025-03-17 13:52:26 -0600

    fix: type errors

--- src/components/blocks/form-block/message/index.tsx
+++ src/components/blocks/form-block/message/index.tsx
@@ -5,9 +5,5 @@ import { Width } from '../width'
 import { SerializedEditorState } from '@payloadcms/richtext-lexical/lexical'
 
 export const Message: React.FC<{ message: SerializedEditorState }> = ({ message }) => {
-  return (
-    <Width className="my-12" width="100">
-      {message && <RichText data={message} />}
-    </Width>
-  )
+  return <Width width={100}>{message && <RichText data={message} />}</Width>
 }
