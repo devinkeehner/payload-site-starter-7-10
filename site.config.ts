53bb307f0aedad964e4c66acf0a8d9cc5ab8ff5e
Bridger Tower <bridgertower@gmail.com>
2025-03-18 08:48:38 -0600

    site config added

--- site.config.ts
+++ site.config.ts
@@ -1,4 +1,10 @@
-export const config = {
+type Config = {
+  name: string
+  url: string
+  description: string
+}
+
+export const config: Config = {
   name: 'Payload Site Starter ✴︎',
   url: 'https://payload-site-starter.vercel.app',
   description: 'Opinionated starter for building websites with Payload and Next.js',
