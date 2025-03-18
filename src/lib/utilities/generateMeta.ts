ef2ae3ac7e4d1d21643278f4094956818c2a58dd
Bridger Tower <bridgertower@gmail.com>
2025-03-18 09:28:20 -0600

    feat: use next js default og image setup

--- src/lib/utilities/generateMeta.ts
+++ src/lib/utilities/generateMeta.ts
@@ -1,31 +1,14 @@
 import type { Metadata } from 'next'
 
-import type { Media, Page, Post, Config } from '@/payload-types'
+import type { Page, Post } from '@/payload-types'
 
 import { mergeOpenGraph } from './mergeOpenGraph'
-import { getServerSideURL } from './getURL'
 import { config } from '@/site.config'
-
-const getImageURL = (image?: Media | Config['db']['defaultIDType'] | null) => {
-  const serverUrl = getServerSideURL()
-
-  let url = serverUrl + '/website-template-OG.webp'
-
-  if (image && typeof image === 'object' && 'url' in image) {
-    const ogUrl = image.sizes?.og?.url
-
-    url = ogUrl ? serverUrl + ogUrl : serverUrl + image.url
-  }
-
-  return url
-}
 
 export const generateMeta = async (args: {
   doc: Partial<Page> | Partial<Post> | null
 }): Promise<Metadata> => {
   const { doc } = args
-
-  const ogImage = getImageURL(doc?.meta?.image)
 
   const title = doc?.meta?.title ? doc?.meta?.title + ' | ' + config.name : config.name
 
@@ -33,13 +16,6 @@ export const generateMeta = async (args: {
     description: doc?.meta?.description,
     openGraph: mergeOpenGraph({
       description: doc?.meta?.description || '',
-      images: ogImage
-        ? [
-            {
-              url: ogImage,
-            },
-          ]
-        : undefined,
       title,
       url: Array.isArray(doc?.slug) ? doc?.slug.join('/') : '/',
     }),
