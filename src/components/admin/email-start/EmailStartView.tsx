055e37605a43261df2ecd50089329fb4a2359678
Devin Keehner <devin.keehner@gmail.com>
2026-05-24 19:28:03 -0400

    fix email start

--- src/components/admin/email-start/EmailStartView.tsx
+++ src/components/admin/email-start/EmailStartView.tsx
@@ -5,7 +5,9 @@ import { isSuperUser } from '@/lib/access/isSuperUser'
 import { EmailStartViewClient } from './EmailStartViewClient'
 
 export default function EmailStartView(props: AdminViewServerProps) {
-  if (!props.user || !isSuperUser(props.user)) {
+  const user = props.user || props.initPageResult?.req?.user
+
+  if (!isSuperUser(user)) {
     return <div style={{ padding: 24 }}>Only super admins can create email campaigns.</div>
   }
 
