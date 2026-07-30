9e90962e6635abea21951270649662f761a3eb97
devinkeehner <devin.keehner@gmail.com>
2026-07-30 15:34:14 -0400

    Add bulk media gallery picker

--- src/components/blocks/media-gallery-block/config.ts
+++ src/components/blocks/media-gallery-block/config.ts
@@ -12,6 +12,15 @@ export const MediaGalleryBlockConfig: Block = {
   },
   fields: [
     {
+      name: 'bulkAddGalleryImages',
+      type: 'ui',
+      admin: {
+        components: {
+          Field: '@/components/admin/media/MediaGalleryBulkAddField#MediaGalleryBulkAddField',
+        },
+      },
+    },
+    {
       name: 'images',
       type: 'array',
       minRows: 1,
